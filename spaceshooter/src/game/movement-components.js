5a01c1390c38ec604cfd5aec60bca08540200792
Akseli V <akseli.virtanen@sek.fi>
2016-03-09 17:04:26 +0200

    naming experiment

--- spaceshooter/src/game/movement-components.js
+++ spaceshooter/src/game/movement-components.js
@@ -3,28 +3,28 @@ game.module(
 )
 .body(function() {
     game.BasicMovement = {
-        init: function() {
+        init: function init() {
             this.body.velocity.y = this.speed;
         },       
-        move: function() {
+        move: function move() {
             
         },
 
-        bark: function() {
+        bark: function bark() {
             
         }
     }
     
     game.BasicMovement2 = {
-        init: function() {
+        init: function init() {
             this.body.velocity.y = this.speed*1.5;
             this.body.velocity.x = _.random(-500, 500);
         },       
-        move: function() {
+        move: function move() {
             
         },
 
-        bark: function() {
+        bark: function bark() {
             
         }
     }

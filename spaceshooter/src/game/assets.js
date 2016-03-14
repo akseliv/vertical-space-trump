fb38530615aeb7ceb48eb8cec4e6d82ac036b5cc
Akseli Virtanen <akseli.virtanen@nearme.fi>
2016-03-14 02:28:29 +0200

    added new assets

--- spaceshooter/src/game/assets.js
+++ spaceshooter/src/game/assets.js
@@ -4,10 +4,12 @@ game.module(
 .body(function() {
     
 game.addAsset('desert-backgorund-looped.png');
+game.addAsset('desert-backgorund-looped-2.png');
 game.addAsset('enemy-small.png');
 game.addAsset('explosion.png');
 game.addAsset('fire.png');
 game.addAsset('laser-bolts.png');
+game.addAsset('enemy-laser-bolts.png');
 game.addAsset('ship.png');
 
 });
